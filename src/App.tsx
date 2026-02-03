4babe46abf2f6aaea8da2a23c2216fa4d4a3f297
Ahmed Badawy <ahmedbadawix77x@gmail.com>
2026-02-03 18:29:58 +0200

    Fix: Configure base path for GitHub Pages and add deployment workflow

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Shopery">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/shop" element={<Shop />} />
